c44c30e3b7b86ff0a84bfae1eb2fcc4b3954b193
Sbor77 <sborovkov82@mail.ru>
2021-05-04 00:43:43 +0300

    Init project commit

---
+++ Assets/Scripts/GetEventOnCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GetEventOnCollision : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _reached = new UnityEvent();
+
+    public bool IsReached { get; private set; }
+
+    public event UnityAction Reached
+    {
+        add => _reached.AddListener(value);
+        remove => _reached.RemoveListener(value);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsReached)
+            return;
+
+        if (collision.TryGetComponent<Thief>(out Thief thief))
+        {
+            IsReached = true;
+            _reached.Invoke();            
+        }
+    }
+}
---
+++ Assets/Scripts/Thief.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Thief : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _hit;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.TryGetComponent(out Door door))
+        {            
+            _hit?.Invoke();
+        }
+    }
+}
